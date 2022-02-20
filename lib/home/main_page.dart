5c7883842522d6b9fe8da7bdd9070fd03e6ce3b7
Naz <80106028+Eduardo79Silva@users.noreply.github.com>
2022-02-20 18:04:07 +0000

    Initial Commit

---
+++ lib/home/main_page.dart
@@ -0,0 +1,48 @@
+import 'package:clinic_app/widgets/BigText.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 45),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [BigText(text: "Clínica Hora Sã", color: AppColors.mainColor)],
+                  ),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(Icons.search, color: Colors.white,),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      color: AppColors.mainColor,
+
+                    ),
+
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
