e0fad6acfeb4a36c075c18dcc0a1f679a975f7fc
Naz <up202005283@up.pt>
2022-02-21 20:47:41 +0000

    Sliding done

--- lib/home/main_page_services.dart
+++ lib/home/main_page_services.dart
@@ -1,5 +1,6 @@
 import 'package:clinic_app/utils/colors.dart';
 import 'package:clinic_app/widgets/BigText.dart';
+import 'package:clinic_app/widgets/IconsText.dart';
 import 'package:clinic_app/widgets/SmallText.dart';
 import 'package:flutter/material.dart';
 
@@ -11,64 +12,142 @@ class ServicesBody extends StatefulWidget {
 }
 
 class _ServicesBodyState extends State<ServicesBody> {
-
   PageController pageController = PageController(viewportFraction: 0.8);
+  var _currPageValue = 0.0;
+  var _height = 220;
+  double _scaleFactor = 0.8;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
       child: PageView.builder(
-
         controller: pageController,
         itemCount: 5,
-        itemBuilder: (context, position){
+        itemBuilder: (context, position) {
           return _builderPageItem(position);
         },
       ),
     );
   }
-  Widget _builderPageItem(int index){
-    return Stack(
 
-      children: [ Container(
-        height: 220,
-        margin: EdgeInsets.only(left: 5, right: 5),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(
-              "assets/image/food0.png"
-            )
-          )
+  Widget _builderPageItem(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+
+    if(index == _currPageValue.floor()){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else if(index == _currPageValue.floor()+1){
+      var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else if(index == _currPageValue.floor()-1){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else{
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 0);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/image/food0.png"))),
         ),
-      ),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 25),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-              color: Colors.white
-
-            ),
+                borderRadius: BorderRadius.circular(30), color: Colors.white),
             child: Container(
               padding: EdgeInsets.only(top: 10, left: 15, right: 15),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   BigText(text: "Fisioterapeuta"),
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Row(
                     children: [
-                      Wrap(children:
-                        List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15,))
+                      Wrap(
+                          children: List.generate(
+                              5,
+                              (index) => Icon(
+                                    Icons.star,
+                                    color: AppColors.mainColor,
+                                    size: 15,
+                                  ))),
+                      SizedBox(
+                        width: 10,
                       ),
-                      SizedBox(width: 10,),
-                      SmallText(text: "4.5")
+                      SmallText(text: "4.5"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "1287"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "comments")
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: IconAndTextWidget(
+                              icon: Icons.circle,
+                              text: "Normal",
+                              iconColor: AppColors.iconColor1)),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                          child: IconAndTextWidget(
+                              icon: Icons.location_pin,
+                              text: "1.7km",
+                              iconColor: AppColors.mainColor)),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                          child: IconAndTextWidget(
+                              icon: Icons.access_time_rounded,
+                              text: "11:00 - 19:30",
+                              iconColor: AppColors.iconColor2)),
                     ],
                   )
                 ],
@@ -76,7 +155,7 @@ class _ServicesBodyState extends State<ServicesBody> {
             ),
           ),
         ),
-    ]);
+      ]),
+    );
   }
 }
-
--- lib/widgets/SmallText.dart
+++ lib/widgets/SmallText.dart
@@ -9,18 +9,21 @@ class SmallText extends StatelessWidget {
   SmallText({Key? key, this.color = const Color(0xFFccc7c5), required this.text,
     this.size=12,
     this.height = 1.2,
-    this.overFlow=TextOverflow.fade}) : super(key: key);
+    this.overFlow=TextOverflow.ellipsis}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style:TextStyle(
-          color: color,
-          fontSize: size,
-          overflow: overFlow,
-          fontFamily: 'Roboto',
-          fontWeight: FontWeight.w100
+    return Flexible(
+      child: Text(
+        text,
+        softWrap: false,
+        style:TextStyle(
+            color: color,
+            fontSize: size,
+            overflow: overFlow,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w100
+        ),
       ),
     );
   }
